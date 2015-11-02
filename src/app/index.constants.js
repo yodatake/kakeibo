868c119c3067ac073aad178a7dd661c88daa374e
依田　剛 <takeshi@YodakeMBA.local>
2015-11-02 20:27:12 +0900

    Initial Commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('kakeibo')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('kakeibo', ['ngAnimate', 'ngCookies', 'ngMessages', 'restangular', 'ui.router', 'toastr']);
+
+})();
